9a797e9140a44f932c7cf3035da878f249eeb48d
Сергеенков Михаил <sergeenkov.mihail@gmail.com>
2017-03-07 12:25:27 +0300

    Исправленные замечания

--- lesson_1/3.rb
+++ lesson_1/3.rb
@@ -1,21 +1,20 @@
 puts 'Введите длины сторон треугольника.'
 puts 'Введите длину первой стороны треугольника:'
-first = gets.chomp.to_i
+first = gets.chomp.to_f
 
 puts 'Введите длину второй стороны треугольника:'
-second = gets.chomp.to_i
+second = gets.chomp.to_f
 
 puts 'Введите длину третьей стороны треугольника:'
-third = gets.chomp.to_i
+third = gets.chomp.to_f
 
 array = [first, second, third].sort
 
-result = if array[0]**2 + array[1]**2 == array[2]**2 && array[0] == array[1]
-  'Треугольник прямоугольный и равнобедренный'
-elsif array[0]**2 + array[1]**2 == array[2]**2
-  'Треугольник прямоугольный'
-else
-  'Треугольник непрямоугольный'
-end
+rectangular = array[0]**2 + array[1]**2 == array[2]**2
 
-puts result
+result_rectangular = rectangular ? 'Треугольник прямоугольный' : 'Треугольник непрямоугольный'
+
+result_equilateral = array[0] == array[1] ? 'Треугольник равнобедренный' : 'Треугольник неравнобедренный'
+
+puts result_rectangular
+puts result_equilateral
